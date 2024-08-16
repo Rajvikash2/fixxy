23137ed32cf9ce820bae9aac1eda93d16beb96b2
Raj <rv2004bolt@gmail.com>
2024-08-16 13:13:09 +0530

    update status endpoint added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-
+import { cors } from 'hono/cors';
 type admin ={
   AdminId:number;
   Username:string;
@@ -28,6 +28,23 @@ const app = new Hono<{Bindings:Bindings}>()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+// CORS Middleware
+
+app.use('*', async (c, next) => {
+
+  // CORS middleware configuration
+  const corsMiddleware = cors({
+    origin: ['http://localhost:5173'],
+    allowHeaders: ['Origin', 'Content-Type', 'Authorization'],
+    allowMethods: ['GET', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+
+  // Apply CORS middleware to all routes to allow cross-origin requests
+  return await corsMiddleware(c, next)
+})
+
 
 //Admin table posting
 app.post('/admin',async(c)=>{
@@ -95,6 +112,54 @@ app.post('/complaints', async (c) => {
     return c.json({ error: error?.toString() });
   }
 });
+
+//retrieve all the complaints
+
+app.get('/posts',async (c)=>{
+  try{
+    const posts = await c.env.DB.prepare('SELECT * FROM Posts WHERE Status = ?').bind('pending').all();
+    return c.json({posts});
+  }
+  catch(error){
+    return c.json({error:error?.toString()});
+  }
+})
+
+//retrieve specidic complaints
+
+
+app.get('/posts/:GoogleId', async (c) => {
+  try {
+    const GoogleId = c.req.param('GoogleId');
+    const post = await c.env.DB.prepare('SELECT * FROM Posts WHERE GoogleId = ?').bind(GoogleId).first();
+    return c.json({ posts: { results: post ? [post] : [] } });
+  } catch (error) {
+    return c.json({ error: error?.toString() });
+  }
+});
+
+//Update the status of specific complaint
+
+app.put('/posts/:GoogleId', async (c) => {
+  try {
+    const GoogleId = c.req.param('GoogleId');
+    const currentTimestamp = new Date().toISOString(); // Get current time in ISO format
+
+    const { success, error } = await c.env.DB.prepare(
+      'UPDATE Posts SET Status = ?, UpdatedAt = ? WHERE GoogleId = ?'
+    ).bind('Resolved', currentTimestamp, GoogleId).run();
+
+    if (success) {
+      return c.json({ message: 'Complaint updated successfully' });
+    } else {
+      return c.json({ message: 'Complaint not updated', error: error || 'Unknown error' });
+    }
+  } catch (error) {
+    return c.json({ error: error?.toString() });
+  }
+});
+
+
 
 
 export default app
